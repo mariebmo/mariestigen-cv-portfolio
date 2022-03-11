1a60c2c7c24b38b9af951e17d21736d30aa85966
mariestigen <marie.stigen@hotmail.com>
2022-03-11 20:11:56 +0100

    ???

--- client/app.jsx
+++ client/app.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CvCard, Links, TopBar } from "./modules";
 import { fetchJSON } from "./http";
-import {current} from "./dist/index.d498c491";
 
 export function App() {
   return (
